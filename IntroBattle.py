5bf56aa5dc6122a37c4b1325b7900830cbd34e75
unknown <lhrscopel@gmail.com>
2021-10-12 20:40:28 -0300

    background configurado e título na tela

--- IntroBattle.py
+++ IntroBattle.py
@@ -16,21 +16,31 @@ class Janela:
 #criar funções
 
 #criar listas
-nomeimg = ['background', 'huntersprite'] #nomes da variáveis das imagens
-locimg = [r"imagens\background\teste 3 ps.png", r"imagens\personagens\hunter sprite.png"] #localização das imagens
+nomeimg = ['background', 'huntersprite', 'menu'] #nomes da variáveis das imagens
+locimg = [r"imagens\background\teste 1.png", r"imagens\personagens\hunter sprite.png", r"imagens\UI\introcomp_menu.png"] #localização das imagens
 #um nome em dada posição x remete a localizacao na posição x
+
+#definindo cores
+PRETO = (0, 0, 0)
+BRANCO = (255, 255, 255)
+VERMELHO = (255, 0, 0)
+VERDE = (0, 255, 0)
+AZUL = (0, 0, 255)
 
 #criando as imagens
 for c in range(len(nomeimg)): #esse loop cria as variáveis das imagens com base nos nome e localização...
     globals()[nomeimg[c]] = pygame.image.load(locimg[c]) #colocados nas listas nomeimg e locimg
 
 #ajustando o tamanho das imagens
-huntersprite = pygame.transform.scale(huntersprite, (150, 150)) #um mero exemplo pra salvar o comando
+huntersprite = pygame.transform.scale(huntersprite, (140, 140)) #um mero exemplo pra salvar o comando
+menu = pygame.transform.scale(menu, (180, 180))
 
 #cria tela
 j = Janela(768,1024) #objeto que guarda e retorna as dimensões da tela
 tela = pygame.display.set_mode(j.dim())
 pygame.display.set_caption("IntroBattle") #estabelece o nome da janela
+largura_janela = tela.get_width() #armazena a largura da janela
+altura_janela = tela.get_height() #armazena a altura da janela
 
 #cria o relógio que controla o fps
 clock = pygame.time.Clock()
@@ -45,8 +55,20 @@ pygame.init()
 while run:
     #coloca o background
     tela.fill(j.cor())
-    tela.blit(background,(0,0))
-    tela.blit(huntersprite,(100,500))
+    tela.blit(background,(0, 0))
+    # tela.blit(menu, (100, 500)) #ainda vamos configurar certinho
+    # tela.blit(huntersprite, (100, 500)) #ainda vamos configurar certinho
+
+    #criando o título "INTROBATTLE!" na tela
+    pygame.draw.rect(tela, BRANCO, pygame.Rect(largura_janela // 2 - 175, 35, 350, 80))
+    pygame.draw.rect(tela, PRETO, pygame.Rect(largura_janela // 2 - 170, 40, 340, 70))
+
+    titulo = 'INTROBATTLE!'
+    pygame.font.init() 
+    fonte = pygame.font.get_default_font() #pega fonte padrão do pygame
+    fontesys = pygame.font.SysFont(fonte, 50) #usa fonte padrão
+    titulotela = fontesys.render(titulo, 1, (255,255,255)) #define nome, tamanho, cor
+    tela.blit(titulotela,(largura_janela // 2 - 130, 60)) # coloca na tela
 
     #coleta os eventos
     for evento in pygame.event.get():
